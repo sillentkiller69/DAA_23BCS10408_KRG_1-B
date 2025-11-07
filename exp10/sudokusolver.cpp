cd11a53454f5891c3e4463f225c2ec366b597e16
sillentkiller69 <jayshankarkrsingh8877@gmail.com>
2025-11-07 16:41:22 +0530

    kj

--- exp10/sudokusolver.cpp
+++ exp10/sudokusolver.cpp
@@ -1,140 +1,176 @@
- #include<iostream>
-#include<fstream>
-#include<cstdlib>
-#include<string>
+#include<bits/stdc++.h>
 using namespace std;
-class SG{
-int g[9][9];
-public:
-SG(){m();}
-void m(){
-cout<<"\n======================\n"<<" 	Sudoku Solver\n"<<"======================\n\n"<<"Welcome to Sudoku Solver!\n"<<"You can either:\n"<<"1. Enter values manually.\n"<<"2. Read puzzle from file.\n"<<"-> ";
-int o;
-cin>>o;
-if(cin.fail()){
-cin.clear();
-cin.ignore(10000,'\n');
-o=0;
+
+// Define the size of the Sudoku board
+const int N = 9;
+const int UNASSIGNED = 0;
+
+// Utility function to print the board
+void printBoard(const vector<vector<int>>& board) {
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < N; col++) {
+            if(col==3||col==6) cout << "| "; 
+            cout << board[row][col] << " ";
+        }
+        cout << endl;
+        if(row==2||row==5) cout << "------+------+-------" << endl;
+    }
+    cout << endl;
 }
-if(o==1)rc();
-else if(o==2)rf();
-else{
-cout<<"Invalid option. Try again.\n";
-m();
+
+// Check if a number is safe to place in the cell
+bool isSafe(const vector<vector<int>>& board, int row, int col, int num) {
+    // Check the row
+    for (int x = 0; x < N; x++) {
+        if (board[row][x] == num) {
+            return false;
+        }
+    }
+    // Check the column
+    for (int x = 0; x < N; x++) {
+        if (board[x][col] == num) {
+            return false;
+        }
+    }
+    // Check the 3x3 sub-grid
+    int startRow = row - row % 3;
+    int startCol = col - col % 3;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board[i + startRow][j + startCol] == num) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
+
+// Recursive backtracking function to solve the Sudoku puzzle
+bool solveSudoku(vector<vector<int>>& board) {
+    int row, col;
+    bool emptyCell = false;
+
+    // Find an unassigned (empty) cell
+    for (row = 0; row < N; row++) {
+        for (col = 0; col < N; col++) {
+            if (board[row][col] == UNASSIGNED) {
+                emptyCell = true;
+                break;
+            }
+        }
+        if (emptyCell) break;
+    }
+
+    // No empty cells means the puzzle is solved
+    if (!emptyCell) {
+        return true;
+    }
+
+    // Try placing numbers 1-9 in the current cell
+    for (int num = 1; num <= N; num++) {
+        if (isSafe(board, row, col, num)) {
+            board[row][col] = num;
+            if (solveSudoku(board)) {
+                return true;
+            }
+            // Backtrack if placing num does not lead to a solution
+            board[row][col] = UNASSIGNED;
+        }
+    }
+    return false; // Trigger backtracking
 }
-void rc(){
-cout<<"\nEnter 0 for empty cells.\n";
-for(int i=0;i<9;i++){
-for(int j=0;j<9;j++){
-int v;
-cout<<"Enter value for cell["<<i+1<<"]["<<j+1<<"] -> ";
-cin>>v;
-while(v<0||v>9){
-cout<<"Invalid value! Enter between 0–9 -> ";
-cin>>v;
+
+// Function to check if the Sudoku puzzle is solved
+bool isSolved(const vector<vector<int>>& board) {
+    for (int row = 0; row < N; row++) {
+        for (int col = 0; col < N; col++) {
+            if (board[row][col] == UNASSIGNED) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
-g[i][j]=v;
+
+// Function to generate a partially filled Sudoku board
+void generateSudoku(vector<vector<int>>& board) {
+    // Fill diagonal 3x3 subgrids to ensure a valid puzzle structure
+    for (int i = 0; i < N; i += 3) {
+        for (int row = i; row < i + 3; row++) {
+            for (int col = i; col < i + 3; col++) {
+                int num;
+                do {
+                    num = rand() % 9 + 1; // Random number between 1 and 9
+                } while (!isSafe(board, row, col, num));
+                board[row][col] = num;
+            }
+        }
+    }
+    // Solve the board using the backtracking algorithm
+    solveSudoku(board);
+
+    // Remove random elements to create a solvable puzzle
+    int removeCount = 40; // Number of cells to remove
+    while (removeCount > 0) {
+        int row = rand() % N;
+        int col = rand() % N;
+        if (board[row][col] != UNASSIGNED) {
+            board[row][col] = UNASSIGNED;
+            removeCount--;
+        }
+    }
 }
-cout<<"-------\n";
-}
-}
-void rf(){
-string f;
-cout<<"\nEnter filename -> ";
-cin>>f;
-ifstream in(f);
-if(!in){
-cout<<"Error opening file!\n";
-exit(1);
-}
-for(int i=0;i<9;i++)
-for(int j=0;j<9;j++){
-int v;
-if(!(in>>v)||v<0||v>9){
-cout<<"Invalid data in file.\n";
-exit(1);
-}
-g[i][j]=v;
-}
-in.close();
-}
-int gv(int r,int c)const{return g[r][c];}
-void sv(int r,int c,int v){g[r][c]=v;}
-bool iF()const{
-for(int i=0;i<9;i++)
-for(int j=0;j<9;j++)
-if(g[i][j]==0)
-return false;
-return true;
-}
-void d()const{
-cout<<"++=====================================++";
-for(int i=0;i<9;i++){
-cout<<"\n||";
-for(int j=0;j<9;j++){
-cout<<" ";
-if(g[i][j])cout<<g[i][j]<<" ";
-else cout<<"  ";
-if((j+1)%3==0)cout<<"||";
-}
-if((i+1)%3==0)
-cout<<"\n++=====================================++";
-else
-cout<<"\n++-----------++-----------++-----------++";
-}
-cout<<endl;
-}
-};
-class SS{
-SG g;
-long long rc=0;
-bool iV(int r,int c,int v){
-for(int i=0;i<9;i++)
-if(g.gv(r,i)==v||g.gv(i,c)==v)
-return false;
-int sr=(r/3)*3,sc=(c/3)*3;
-for(int i=sr;i<sr+3;i++)
-for(int j=sc;j<sc+3;j++)
-if(g.gv(i,j)==v)
-return false;
-return true;
-}
-bool sR(){
-rc++;
-for(int r=0;r<9;r++){
-for(int c=0;c<9;c++){
-if(g.gv(r,c)==0){
-for(int v=1;v<=9;v++){
-if(iV(r,c,v)){
-g.sv(r,c,v);
-if(sR())return true;
-g.sv(r,c,0);
-}
-}
-return false;
-}
-}
-}
-return true;
-}
-public:
-SS(){
-cout<<"\nOriginal Puzzle:\n";
-g.d();
-cout<<"\nSolving puzzle...\n";
-bool s=sR();
-cout<<"\nThe solver was called "<<rc<<" times.\n";
-if(s){
-cout<<"\nSolved Puzzle:\n";
-g.d();
-cout<<"\nPuzzle solved successfully!\n";
-}else{
-cout<<"\nNo valid solution exists.\n";
-}
-}
-};
-int main(){
-SS s;
-return 0;
+
+// Main function to run the game
+int main() {
+    srand(time(0)); // Initialize random seed
+
+    // Initialize the Sudoku board with zeros (empty cells)
+    vector<vector<int>> board(N, vector<int>(N, UNASSIGNED));
+
+    // Generate a Sudoku puzzle
+    generateSudoku(board);
+
+    cout << "Generated Sudoku Puzzle:" << endl;
+    printBoard(board);
+
+    char choice;
+    do {
+        cout << "Options: " << endl;
+        cout << "1. Solve the puzzle automatically" << endl;
+        cout << "2. Enter your solution" << endl;
+        cout << "Enter option (1 or 2): ";
+        cin >> choice;
+
+        if (choice == '1') {
+            // Solve the puzzle automatically using backtracking
+            if (solveSudoku(board)) {
+                cout << "Solved Sudoku Puzzle:" << endl;
+                printBoard(board);
+            } else {
+                cout << "No solution exists." << endl;
+            }
+        } else if (choice == '2') {
+            int row, col, num;
+            cout << "Enter row (1-9), column (1-9), and number (1-9) to insert: ";
+            cin >> row >> col >> num;
+
+            if (isSafe(board, row - 1, col - 1, num)) {
+                board[row - 1][col - 1] = num;
+                cout << "Updated Sudoku Puzzle:" << endl;
+                printBoard(board);
+            } else {
+                cout << "Invalid move. Try again." << endl;
+            }
+
+            if (isSolved(board)) {
+                cout << "Congratulations! You solved the Sudoku puzzle!" << endl;
+                break;
+            }
+        } else {
+            cout << "Invalid choice. Please enter 1 or 2." << endl;
+        }
+    } while (true);
+
+    return 0;
 }
